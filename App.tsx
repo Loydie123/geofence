7148550230aea97b09d6369862a361256caf9414
Loydie123 <loydie319@gmail.com>
2025-07-26 14:18:17 +0800

    add a desc text

--- App.tsx
+++ App.tsx
@@ -6,12 +6,15 @@ import './global.css';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-start bg-white pt-40">
-      <View className="items-center space-y-4">
+      <View className="items-center space-y-6">
         <Image 
           source={require('./assets/images/geofence.png')}
           className="h-96 w-96"
           resizeMode="contain"
         />
+        <Text className="text-xl text-gray-700 font-medium text-center px-4">
+          Real Time alert for smooth safe commute
+        </Text>
       </View>
       <StatusBar style="auto" />
     </View>
