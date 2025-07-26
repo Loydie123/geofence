c2eca699e094ccd2c8bdce9c021b9fe1126c3a3a
Loydie123 <loydie319@gmail.com>
2025-07-26 14:13:03 +0800

    landing page:

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-xl font-bold">CommuteSafe</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <View className="items-center space-y-4">
+        <Image 
+          source={require('./assets/images/geofence.png')}
+          className="h-64 w-64"
+          resizeMode="contain"
+        />
+        <Text className="text-xl font-bold text-gray-800">CommuteSafe</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
