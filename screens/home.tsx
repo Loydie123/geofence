6121836f0c3b6ab0d7d20c91be8200c2a535f83a
Loydie123 <loydie319@gmail.com>
2025-07-27 15:43:02 +0800

    container

--- screens/home.tsx
+++ screens/home.tsx
@@ -28,7 +28,6 @@ export default function HomeScreen() {
         shadowRadius: 4,
         elevation: 5,
       }}>
-        {/* Handle Line */}
         <View style={{
           width: 40,
           height: 4,
