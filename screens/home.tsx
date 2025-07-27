1bc81b5dd6f430243cdcbb8901f77b5120a9dff9
Loydie123 <loydie319@gmail.com>
2025-07-27 15:40:50 +0800

    add container

--- screens/home.tsx
+++ screens/home.tsx
@@ -7,6 +7,28 @@ export default function HomeScreen() {
     <View style={{ flex: 1 }}>
       <MapScreen />
       <MapButtons />
+      
+      {/* Bottom Container */}
+      <View style={{
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        backgroundColor: 'white',
+        height: 200,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        padding: 20,
+        shadowColor: '#000',
+        shadowOffset: {
+          width: 0,
+          height: -4,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+      }}>
+      </View>
     </View>
   );
 } 
